88da92342870495fd62c6f9078fd03295ea24ffb
Syan Delbart <syan.delbart@hotmail.com>
2022-10-01 22:18:01 +0200

    Changed navbar to next/link elements

--- components/navbar.js
+++ components/navbar.js
@@ -1,8 +1,14 @@
-function Navbar() {
+import Link from 'next/link'
+
+const Navbar = () => {
   return (
     <nav className="fixed top-0 right-0 p-5 z-50 child:mx-2 child:lowercase child:after:absolute child:after:bg-black child:after hover:child:after:w-6/12 hover:child:after:text-green-500">
-        <a href="/">Home</a>
-        <a href="/projects">Projects</a>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/projects">
+          <a>Projects</a>
+        </Link>
     </nav>
   );
 }
