556b68300f00f9456afb8c625307e22fc9e8ee99
Syan Delbart <syan.delbart@hotmail.com>
2022-10-03 15:36:47 +0200

    Formatted footer, for now

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,17 @@
 function Footer() {
   return (
-    <h1>footer</h1>
+    <footer>Made with lots of ❤️ by Syan Delbart - &copy; 2022
+      <style jsx>{`
+        footer {
+          width: 100%;
+          text-align: center;
+          position: absolute;
+          bottom: 0;
+          padding: 20px 0;
+          color: rgb(57, 92, 61);
+      `}</style>
+    </footer>
+
   );
 }
 
