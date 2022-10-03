f15b8bbfb284e928b741123ccb91ca1f558b8704
Syan Delbart <syan.delbart@hotmail.com>
2022-10-03 15:40:10 +0200

    Removed unused dependency

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CS
 import "@fortawesome/free-brands-svg-icons";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
-import Script from "next/script";
 
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
