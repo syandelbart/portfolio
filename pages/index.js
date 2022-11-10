9b392704837ffce8dd1f66d5feba83c4997a4298
Syan Delbart <syan.delbart@hotmail.com>
2022-11-10 22:36:01 +0100

    mobile support

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export async function getStaticProps({ locale }) {
 export default function Home() {
   const { t } = useTranslation('home');
   return (
-      <div className="h-screen max-h-screen w-screen flex flex-col justify-center items-center relative bg-background" >
+      <div className="min-h-screen w-screen flex flex-col justify-center items-center relative bg-background " >
         <Head>
           <title>Home - syandelbart.com</title>
           <meta property="og:title" content="Home - syandelbart.com" />
@@ -41,18 +41,18 @@ export default function Home() {
           />
         </Head>
 
-        <div className="flex text-default justify-around w-full  max-w-6xl ">
-          <div className="flex flex-col max-w-[33vw] items-start">
-            <h1 className="text-4xl font-bold">{t("intro.title")}</h1>
-            <p className="text-xl my-2 max-w-[300px]">
+        <div className="flex text-default justify-center sm:justify-around m-auto w-full max-w-[95vw]  sm:max-w-6xl flex-col sm:flex-row mt-40 sm:mt-0 mb-20 sm:mb-0">
+          <div className="flex flex-col sm:max-w-[33vw] items-start m-auto sm:m-0">
+            <h1 className="text-3xl sm:text-4xl font-bold">{t("intro.title")}</h1>
+            <p className="text-lg sm:text-xl my-2 max-w-[75vw] sm:max-w-[300px]">
               {t("intro.description")}
             </p>
-            <Link href="/" className="bg-backdrop py-3 px-10 text-2xl rounded-md my-8">
+            <Link href="/" className="bg-backdrop py-2 sm:py-3 px-4 sm:px-10 text-2xl rounded-md sm:my-8 m-auto mt-5">
               {t("intro.button")}
             </Link>
             
           </div>
-          <div className="flex flex-col justify-center gap-2 items-center text-xl text-soft">
+          <div className="flex flex-row sm:flex-col justify-center gap-2 items-center text-xl text-soft my-10 sm:my-0">
             <a className="relative" target="_blank" href="https://www.linkedin.com/in/syandelbart/">
             <FontAwesomeIcon style={{ fontSize:30}} icon={faLinkedin} />
               <span className="tooltip">Linkedin</span>
@@ -76,7 +76,7 @@ export default function Home() {
                 alt="A picture of me, Syan"
                 width={300}
                 height={300}
-                className="border-[10px]  border-backdrop"
+                className="border-[10px] border-backdrop m-auto"
               />
           </div>
 
