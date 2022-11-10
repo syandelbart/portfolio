c2e3b7613d81ffcc39cb5777b819610e4a36e076
Syan Delbart <syan.delbart@hotmail.com>
2022-11-10 19:29:46 +0100

    config files for coming change

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   images : {
     unoptimized: true
+  },
+  i18n: {
+    locales: ['en', 'nl'],
+    defaultLocale: 'en',
+    localeDetection: false,
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
     extend: {
       colors: {
         "primary": "#0e7490",
+        "background": "#222020",
+        "default": "#f5f5f5",
+        "nav-background": "#222020",
+        "nav-text": "#ffffff",
+        "footer-background": "#222020",
+        "footer-text": "#ffffff",
+        "backdrop": "#3E3E3E",
+        "soft": "#BEBEBE",
+        "link-inactive": "#B9B7B7"
       }
     },
   },
